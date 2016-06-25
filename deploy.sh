6b315ff2f7df1bb9213f4883c62f028efd310946
Jamie Hewland <jhewland@gmail.com>
2016-06-25 18:08:07 +0200

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Given a list of images as input...
-IMAGES="$@"
+IMAGES=("$@")
 
 # Parse the Alpine version from the curl Dockerfile
 BASE_IMAGE="$(awk '$1 == "FROM" { print $2; exit }' curl/Dockerfile)"
@@ -10,7 +10,7 @@ ALPINE_VERSION="${BASE_IMAGE##*:}"
 
 docker login -u "$REGISTRY_USER" -p "$REGISTRY_PASS"
 
-for image in "$IMAGES"; do
+for image in "${IMAGES[@]}"; do
   tag="${image##*:}"
   if [[ "$tag" != "latest" ]]; then
     version_tag="$ALPINE_VERSION-$tag"
