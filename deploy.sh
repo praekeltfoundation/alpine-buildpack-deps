698b66b58d4f811ee0de9512a3fd7ecbe8d8aa74
Jamie Hewland <jhewland@gmail.com>
2016-06-25 17:30:20 +0200

    Rather parse images to be pushed in Travis file
    * Specific to Travis

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-# Get all the images in Docker with the right name
-IMAGE="$REGISTRY_USER/alpine-buildpack-deps"
-IMAGES=($(docker images --format '{{.Repository}}:{{.Tag}}' | grep "$IMAGE"))
+# Given a list of images as input...
+IMAGES="$@"
 
 # Parse the Alpine version from the curl Dockerfile
 BASE_IMAGE="$(awk '$1 == "FROM" { print $2; exit }' curl/Dockerfile)"
@@ -11,7 +10,7 @@ ALPINE_VERSION="${BASE_IMAGE##*:}"
 
 docker login -u "$REGISTRY_USER" -p "$REGISTRY_PASS"
 
-for image in "${IMAGES[@]}"; do
+for image in "$IMAGES"; do
   tag="${image##*:}"
   if [[ "$tag" != "latest" ]]; then
     version_tag="$ALPINE_VERSION-$tag"
