444b1752ec2a1dd8729c81afb613aa52615b2a4c
Jamie Hewland <jhewland@gmail.com>
2016-06-25 17:24:19 +0200

    Add Travis file and deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+# Get all the images in Docker with the right name
+IMAGE="$REGISTRY_USER/alpine-buildpack-deps"
+IMAGES=($(docker images --format '{{.Repository}}:{{.Tag}}' | grep "$IMAGE"))
+
+# Parse the Alpine version from the curl Dockerfile
+BASE_IMAGE="$(awk '$1 == "FROM" { print $2; exit }' curl/Dockerfile)"
+ALPINE_VERSION="${BASE_IMAGE##*:}"
+
+docker login -u "$REGISTRY_USER" -p "$REGISTRY_PASS"
+
+for image in "${IMAGES[@]}"; do
+  tag="${image##*:}"
+  if [[ "$tag" != "latest" ]]; then
+    version_tag="$ALPINE_VERSION-$tag"
+  else
+    version_tag="$ALPINE_VERSION"
+  fi
+
+  docker-ci-deploy --tag "$tag" "$version_tag" -- "$image"
+done
